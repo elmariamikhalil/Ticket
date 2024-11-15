2535affbc895957e3dd95816ccafd0475b8aaac2
elmariamikhalil <elmariamikhalil@gmail.com>
2024-11-15 14:44:52 +0100

    #

--- client/src/components/TicketItem.jsx
+++ client/src/components/TicketItem.jsx
@@ -4,11 +4,9 @@ function TicketItem({ ticket }) {
   return (
     <div className="ticket">
       <div>{new Date(ticket.createdAt).toLocaleString("en-US")}</div>
-      <div>Category: {ticket.category}</div>
-      {ticket.category === "material" && <div>Option : {ticket.material}</div>}
-      {ticket.category === "environment" && (
-        <div>Option : {ticket.envirement}</div>
-      )}
+      <div>{ticket.category}</div>
+      {ticket.category === "Material" && <div>{ticket.material}</div>}
+      {ticket.category === "Environment" && <div>{ticket.envirement}</div>}
       <div className={`status status-${ticket.status}`}>{ticket.status}</div>
       <Link to={`/ticket/${ticket._id}`} className="btn btn-reverse btn-sm">
         View
